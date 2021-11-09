8d43004aff5260f9ba11ff054cdb09364f3019a4
Leyli Tahmasebi <leylitahmasebi@Leylis-Air-43.lan>
2021-11-09 18:28:32 -0500

    main

--- src/components/Board.js
+++ src/components/Board.js
@@ -1,27 +1,31 @@
-import React from "react";
+import React from 'react';
 import Square from './Square'
 
 export default function Board(props) {
-  const renderSquares = (i) => {
-    return <Square onClick={() => props.onClick(i)} value={props.squares[i]} />;
-  };
-  return (
-    <div className="board">
-      <div className="border-row">
-        {renderSquares(0)}
-        {renderSquares(1)}
-        {renderSquares(2)}
-      </div>
-      <div className="border-row">
-        {renderSquares(3)}
-        {renderSquares(4)}
-        {renderSquares(5)}
-      </div>
-      <div className="border-row">
-        {renderSquares(6)}
-        {renderSquares(7)}
-        {renderSquares(8)}
-      </div>
-    </div>
-  );
-}
+
+const renderSquare = (i) =>{
+        return <Square 
+        onClick={()=>props.onClick(i)}
+        value={props.squares[i]}
+        />
+    }
+    return (
+        <div>
+        <div className="border-row">
+            {renderSquare(0)}
+            {renderSquare(1)}
+            {renderSquare(2)}    
+        </div>
+        <div className="border-row">
+            {renderSquare(3)}
+            {renderSquare(4)}
+            {renderSquare(5)}    
+        </div>
+        <div className="border-row">
+            {renderSquare(6)}
+            {renderSquare(7)}
+            {renderSquare(8)}    
+        </div>
+        </div>
+    )
+};
