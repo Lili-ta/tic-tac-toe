8ab513bc4cb05dc56033c0d4b1120fd53be90372
Leyli Tahmasebi <leylitahmasebi@Leylis-Air-43.lan>
2021-11-09 17:46:35 -0500

    main

--- src/components/Game.js
+++ src/components/Game.js
@@ -2,11 +2,11 @@ import React, { Component } from 'react'
 import Board from './Board'
 
 export default class Game extends Component {
-    state={
-        xIsNext:true,
-        step:0,
-        history: (Array[9].fill(null))
-    }
+    state = {
+        xIsNext: true,
+        step: 0,
+        history: [{ squares: Array(9).fill(null) }],
+      };
     render() {
         return (
             <div>
