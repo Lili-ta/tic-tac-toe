9b738d68fa0ce7be3007af6507c6bdecd07e7a75
Leyli Tahmasebi <leylitahmasebi@Leylis-Air.lan>
2021-11-10 12:46:27 -0500

    main

--- src/components/Game.unit.test.js
+++ src/components/Game.unit.test.js
@@ -1,5 +1,6 @@
 import testHelper from "../testHelper";
 import { fireEvent, waitFor, screen } from "@testing-library/react";
+import * as handler from "../appHelpers";
 import "@testing-library/jest-dom/extend-expect";
 import Game from "./Game";
 
@@ -15,6 +16,9 @@ it("should render Game component and fireevent with click on button", async () =
   });
 
   it("should render value in button after click on button", async () => {
+    let check = jest.spyOn(handler,"calculateWinner");
+    let squares = ["X","O","X","O","X"];
+    let output = check(squares);
     const { getAllByRole, getByText, getAllByText} = testHelper.renderTest(<Game/>);
     expect(getAllByRole("button")).toHaveLength(9);
     fireEvent.click(getAllByRole("button")[0]);
@@ -24,5 +28,10 @@ it("should render Game component and fireevent with click on button", async () =
     expect(getByText("O")).toBeInTheDocument();
     fireEvent.click(getAllByRole("button")[4]);
     expect(getAllByText("X")).toHaveLength(2);
-    
+    fireEvent.click(getAllByRole("button")[2]);
+    expect(getAllByText("O")).toHaveLength(2);
+    fireEvent.click(getAllByRole("button")[8]);
+    expect(getAllByText("X")).toHaveLength(3);
+    fireEvent.click(getAllByRole("button")[3]);
+    expect(output).toEqual("O")
   });
