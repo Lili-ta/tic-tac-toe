e2f96505e4fa447fe2ac77f1941c89fadff1fff8
Leyli Tahmasebi <leylitahmasebi@Leylis-Air-49.lan>
2021-11-10 17:47:59 -0500

    main

--- src/components/Game.unit.test.js
+++ src/components/Game.unit.test.js
@@ -31,3 +31,23 @@ it("should render Game component and fireevent with click on button", async () =
     fireEvent.click(getAllByRole("button")[3]);
     expect(getByText("Winner Is X")).toBeInTheDocument();
   });
+
+  it("should jump to move that clicked", async () => {
+    const { getAllByRole, getByText, getAllByText} = testHelper.renderTest(<Game/>);
+    expect(getAllByRole("button")).toHaveLength(10);
+    fireEvent.click(getAllByRole("button")[0]);
+    expect(getByText("X")).toBeInTheDocument();
+    expect(getByText("Go To # 1")).toBeInTheDocument();
+    expect(getByText("Next Player Is O")).toBeInTheDocument();
+    fireEvent.click(getAllByRole("button")[3]);
+    expect(getAllByText("X")).toHaveLength(1);
+    expect(getByText("O")).toBeInTheDocument();
+    expect(getByText("Go To # 2")).toBeInTheDocument();
+    expect(getByText("Next Player Is X")).toBeInTheDocument();
+    fireEvent.click(getAllByRole("button")[1]);
+    expect(getAllByText("X")).toHaveLength(2);
+    expect(getByText("Go To # 3")).toBeInTheDocument();
+    expect(getByText("Next Player Is O")).toBeInTheDocument();
+    fireEvent.click(getByText("Go To # 2"));
+    expect(getAllByText("X")).toHaveLength(2);
+  });
