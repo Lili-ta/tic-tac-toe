e808a4071e674f9a3d10b08f0c041242e925c0ac
Leyli Tahmasebi <leylitahmasebi@Leylis-Air.lan>
2021-11-10 10:42:08 -0500

    main

--- src/components/Game.unit.test.js
+++ src/components/Game.unit.test.js
@@ -13,3 +13,10 @@ it("should render Game component and fireevent with click on button", async () =
     expect(getAllByRole("button")).toHaveLength(9);
     fireEvent.click(getAllByRole("button")[0]);
   });
+
+  it("should render value in button after click on button", async () => {
+    const { getAllByRole, getByText} = testHelper.renderTest(<Game/>);
+    expect(getAllByRole("button")).toHaveLength(9);
+    fireEvent.click(getAllByRole("button")[0]);
+    expect(getByText("O")).toBeInTheDocument();
+  });
